bb02e4834ffc708b195af485d51cabc99c99b85e
Ngọc Anh <ngdiem39@gmail.com>
2024-06-06 17:03:31 +0700

    Update autobot.js

--- autobot.js
+++ autobot.js
@@ -1,6 +1,11 @@
+const consoleRed = 'font-weight: bold; color: red;';
+const consoleGreen = 'font-weight: bold; color: green;';
+const consolePrefix = '%c [AutoBot] ';
+
+console.clear()
 try {
     function onClickAuto() {
-        u(`AutoBot ${P.cat.isAuto ? 'deactivated' : 'activated'}!\n\nCracked by @clqkx`)
+        u(`AutoBot ${P.cat.isAuto ? 'deactivated' : 'activated'}!\n\nAutoBot by @Tom00702`)
         P.cat.isAuto = !P.cat.isAuto,
         P.cat.isAuto ? (this.ani8.play(0, !0),
         Laya.timer.loop(500, this, this.checkAuto),
@@ -10,9 +15,6 @@ try {
         this.m_img_StopAuto.visible = !P.cat.isAuto
     }
     M.prototype.onClickAuto = onClickAuto
-    console.log(`${consolePrefix}Script loaded`, consoleGreen);
-    console.log(`${consolePrefix}Crack by @clqkx`, consoleGreen);
 } catch (e) {
-    console.log(`${consolePrefix}An error occurred, the BrakePoint is set incorrectly!`, consoleRed);
-    console.log(`${consolePrefix}Please follow the instructions, and you will succeed :*`, consoleRed);
+    throw e;
 }
