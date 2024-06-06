dca102f09c9e813eb0187292b2a8dc532ee0bed1
Ngọc Anh <ngdiem39@gmail.com>
2024-06-06 16:35:43 +0700

    Create autobot.js

---
+++ autobot.js
@@ -0,0 +1,1 @@
+function onClickAuto() { P.cat.isAuto = !P.cat.isAuto, P.cat.isAuto ? (this.ani8.play(0, !0), Laya.timer.loop(500, this, this.checkAuto), this.checkFreeCat()) : (Laya.timer.clearAll(this.checkAuto), this.ani8.stop(), Laya.timer.loop(5e3, this, this.checkSum)), this.m_img_StopAuto.visible = !P.cat.isAuto } M.prototype.onClickAuto = onClickAuto;
