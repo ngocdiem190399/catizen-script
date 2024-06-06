a72be0c264d2c939ea28ff3588a15d1c26f774bd
Ngọc Anh <ngdiem39@gmail.com>
2024-06-06 23:54:32 +0700

    Update autobot.js

--- autobot.js
+++ autobot.js
@@ -1,6 +1,6 @@
 try {
     function onClickAuto() {
-        u(`AutoBot ${P.cat.isAuto ? 'OFF' : 'ON'} by Tom00702 - Vinh Duyet`),
+        u(`AutoBot ${P.cat.isAuto ? 'OFF' : 'ON'} by Tom00702 - Vinh Duyet`);
         P.cat.isAuto = !P.cat.isAuto,
         P.cat.isAuto ? (this.ani8.play(0, !0),
         Laya.timer.loop(500, this, this.checkAuto),
