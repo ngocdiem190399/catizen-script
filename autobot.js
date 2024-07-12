b628f747765c30a8f1194db24ca7d22ce716e1ed
Ngọc Anh <ngdiem39@gmail.com>
2024-07-12 14:39:05 +0700

    Update autobot.js

--- autobot.js
+++ autobot.js
@@ -1,17 +1,17 @@
 try {
     function onClickAuto() {
-        u(`AutoBot ${P.cat.isAuto ? 'OFF' : 'ON'} by Tom00702 - Vinh Duyet`);
+        u(`AutoBot ${P.cat.isAuto ? 'OFF' : 'ON'} by Tom - Vinh Duyet`);
         P.cat.isAuto = !P.cat.isAuto,
         P.cat.isAuto ? (this.ani8.play(0, !0),
         Laya.timer.loop(500, this, this.checkAuto),
-        this.checkFreeCat()) : (Laya.timer.clearAll(this.checkAuto),
+        this.checkFreeCat(),
+        this.m_btn_AutoSetting.visible = !0) : (Laya.timer.clearAll(this.checkAuto),
         this.ani8.stop(),
-        Laya.timer.loop(5e3, this, this.checkSum)),
+        Laya.timer.loop(5e3, this, this.checkSum),
+        this.m_btn_AutoSetting.visible = !0),
         this.m_img_StopAuto.visible = !P.cat.isAuto
     }
-    
     M.prototype.onClickAuto = onClickAuto
-    
 } catch (e) {
  
 }
