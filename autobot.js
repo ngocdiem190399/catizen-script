e407349a9130e539fd3ab4b9770daff67ff11bb1
Ngọc Anh <ngdiem39@gmail.com>
2024-07-20 02:01:10 +0700

    Update autobot.js

--- autobot.js
+++ autobot.js
@@ -1,17 +1,18 @@
 try {
     function onClickAuto() {
-        u(`AutoBot ${P.cat.isAuto ? 'OFF' : 'ON'} by Tom - Vinh Duyet`);
-        P.cat.isAuto = !P.cat.isAuto,
-        P.cat.isAuto ? (this.ani8.play(0, !0),
-        Laya.timer.loop(500, this, this.checkAuto),
-        this.checkFreeCat(),
-        this.m_btn_AutoSetting.visible = !0) : (Laya.timer.clearAll(this.checkAuto),
-        this.ani8.stop(),
-        Laya.timer.loop(5e3, this, this.checkSum),
-        this.m_btn_AutoSetting.visible = !0),
-        this.m_img_StopAuto.visible = !P.cat.isAuto
+        u(`Cracked by Tom - Vinh Duyet`);
+        P.cat.clickAuto = !0;
+        this.m_img_AutoRed.visible = !0;
+	    if (!P.cat.buyAuto) {
+          P.cat.buyAuto = !0;
+    	}
+        P.cat.isAuto = !0;
+        this.ani8.play(0, !0);
+    	Laya.timer.loop(500, this, this.checkAuto);
+    	this.checkFreeCat();
+    	this.m_btn_AutoSetting.visible = !0;
     }
-    M.prototype.onClickAuto = onClickAuto
+    M.prototype.onClickAuto = onClickAuto;
 } catch (e) {
- 
+
 }
